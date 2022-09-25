d0b29cf0c2b6a7e4928fe3dec8998fae0e7ea640
Rafael Ghinato <rga@cesar.school>
2022-09-25 10:15:56 -0300

    Ajustando Primeiro Commit

--- exercicio2.js
+++ exercicio2.js
@@ -1,7 +1,6 @@
-(function () {
-    const arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    const numerosPares = arrayNumeros.filter(valor => valor % 2 == 0);
-    console.log(numerosPares);
-})();
+const numerosPares = arrayNumeros.filter(valor => valor % 2 == 0);
+console.log(numerosPares);
+
 
